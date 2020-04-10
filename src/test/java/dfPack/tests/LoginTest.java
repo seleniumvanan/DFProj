f4309b3cbdb1be6e2f082e00336c2763ae39dfd7
kalaivanan <seleniumvanan@gmail.com>
2020-04-10 20:25:28 +0530

    comment for branch checkout changes to be tested at master get effect or not?

--- src/test/java/dfPack/tests/LoginTest.java
+++ src/test/java/dfPack/tests/LoginTest.java
@@ -19,6 +19,7 @@ import junit.framework.Assert;
 public class LoginTest extends BaseTest {
 	
 	//Comment here for Git testing purpose 
+	//comment for branch checkout changes to be tested at master get effect or not?
 	@BeforeClass
 	public void init() {
 		
