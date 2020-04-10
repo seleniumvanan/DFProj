99b59a1959924a07e127ed4b7a758928d56621d9
kalaivanan <seleniumvanan@gmail.com>
2020-04-10 14:28:00 +0530

    BaseTest.java-> Uncommented and committed

--- src/test/java/dfPack/base/BaseTest.java
+++ src/test/java/dfPack/base/BaseTest.java
@@ -34,7 +34,7 @@ public class BaseTest {
 	  public WebDriver driver=null;
 		
 	  //Initialization
-	//Comment here for Git testing purpose 
+	
 	  public void initialise(){
 		  
 		  if(prop==null){
